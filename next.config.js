6d7eef4a2251d5ebd45caae327cbaec02032cf26
Karl Lee <poogooflupduck@gmail.com>
2022-02-22 16:24:36 +0000

    set default page to real time tx

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/transactions/real-time",
+        permanent: true,
+      },
+    ];
+  },
+};
